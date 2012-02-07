8b1651c1cd47b2cf7cbb1554a344a93697f2d5da
Joseph Hanson <jrh0090@gmail.com>
2012-02-07 09:59:03 -0600

    removed 4.0.5

--- generate.sh
+++ generate.sh
@@ -12,4 +12,4 @@ fi
 if [ ! -h core ]; then
     ln -s $ZENHOME/Products/ZenUI3/browser/resources/js/zenoss core
 fi
-jsduck  --title "Zenoss User Interface API documentation" --images=ext-4.0.5/docs/doc-resources/ ext-4.0.5/src core --guides guides.json -o $OUTPUT
+jsduck  --title "Zenoss User Interface API documentation" --images=ext-4.0.7-gpl/docs/doc-resources/ ext-4.0.7-gpl/src core --guides guides.json -o $OUTPUT
