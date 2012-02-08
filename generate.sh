dabeae5ca09a16e562bcd5b8611bfae2d6a955f7
Joseph Hanson <jrh0090@gmail.com>
2012-02-08 13:44:56 -0600

    fixed the images link

--- generate.sh
+++ generate.sh
@@ -12,6 +12,7 @@ fi
 if [ ! -h core ]; then
     ln -s $ZENHOME/Products/ZenUI3/browser/resources/js/zenoss core
 fi
-jsduck  --title "Zenoss User Interface API documentation" --builtin-classes --welcome=index.html --head-html='<link rel="stylesheet" href="style.css" type="text/css">'  --images=ext-4.0.7-gpl/docs/doc-resources/ ext-4.0.7-gpl/src core --guides guides.json -o $OUTPUT
+jsduck  --title "Zenoss User Interface API documentation" --builtin-classes --welcome=index.html --head-html='<link rel="stylesheet" href="style.css" type="text/css">'  --images=ext-4.0.7-gpl/docs/images ext-4.0.7-gpl/src core --guides guides.json -o $OUTPUT
 # make sure the style css file gets there
 cp style.css $OUTPUT
+cp favicon.ico $OUTPUT
